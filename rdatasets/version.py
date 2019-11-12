2b9b7458a5c631a1a334a926c35c4285b1aa2830
holgern <holgernahrstaedt@gmx.de>
2019-11-12 22:36:13 +0100

    Fix license name in setup.py

--- rdatasets/version.py
+++ rdatasets/version.py
@@ -3,7 +3,7 @@
 short_version = '0.1.0'
 version = '0.1.0'
 full_version = '0.1.0'
-git_revision = 'Unknown'
+git_revision = '5a7dc2fdbe65a651e03b8e396df5918bf77a889c'
 release = True
 
 if not release:
